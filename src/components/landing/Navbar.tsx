f7e061f6cd86290ce01874ccf5abb3c7bde03c5f
l0lxl0lw <l0lxl0lw@gmail.com>
2025-12-24 07:50:32 -0800

    udpating navbar CTA

--- src/components/landing/Navbar.tsx
+++ src/components/landing/Navbar.tsx
@@ -56,7 +56,7 @@ export function Navbar() {
               <Link to="/auth">Log In</Link>
             </Button>
             <Button variant="hero" asChild>
-              <Link to="/auth">Get Started for $1</Link>
+              <Link to="/auth">Get Started</Link>
             </Button>
           </div>
 
@@ -97,7 +97,7 @@ export function Navbar() {
                   <Link to="/auth">Log In</Link>
                 </Button>
                 <Button variant="hero" asChild>
-                  <Link to="/auth">Get Started for $1</Link>
+                  <Link to="/auth">Get Started</Link>
                 </Button>
               </div>
             </div>
